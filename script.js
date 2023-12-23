89dbb139a6694bb6da6471ff7d232a6f3366fdd1
MetaMiku <MetaMiku@hotmail.com>
2023-12-23 21:24:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mouseout Killer
 // @author       MetaMiku
-// @description  禁用学习通视频学习web对鼠标离开的eventlisteners - (修改自keepcalmandbelogical的脚本mouse events ad killer)
+// @description  禁用某某通视频学习web对鼠标离开的eventlisteners - (修改自keepcalmandbelogical的脚本mouse events ad killer)
 // @warning      本脚本请在下载后24h内删除，本脚本未进行后台检验，由此脚本造成的一切后果由使用者自负
 // @version      0.1
 // @match        *://*.chaoxing.com/*
