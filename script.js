7b42c6e926eb4df985e97518bf4d5a60547dd135
MetaMiku <MetaMiku@hotmail.com>
2023-12-23 21:23:42 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Mouseout Killer
+// @author       MetaMiku
+// @description  禁用学习通视频学习web对鼠标离开的eventlisteners - (修改自keepcalmandbelogical的脚本mouse events ad killer)
+// @warning      本脚本请在下载后24h内删除，本脚本未进行后台检验，由此脚本造成的一切后果由使用者自负
+// @version      0.1
+// @match        *://*.chaoxing.com/*
+// @license      MIT
+// ==/UserScript==
+
+(function() {
+    Window.prototype.addEventListener = (function() {
+        var f = Window.prototype.addEventListener;
+        return function(type, handler) {
+            if (type.toLowerCase() !== "mouseout") {
+                f.apply(this, arguments);
+            }
+        };
+    })();
+})();
